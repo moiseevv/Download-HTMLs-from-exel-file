5cba7150d3bca9f4fb29eac6957c2381c73118c1
vmoiseev <vmoiseev@ro-nso.ru>
2021-10-25 11:03:30 +0700

    Ввод занчений номеров сотолбцов добавлен вверх

--- html.py
+++ html.py
@@ -1,17 +1,20 @@
 import requests
 from openpyxl import load_workbook as lwb
+column_kadastr = 5          # Как правило 4 - я
+column_link_on_html = 47    # Как правило 45 - я
+
 
 wb = lwb("Кт.xlsx")
 ws = wb.active
 row_count = ws.max_row
-print(row_count)
+print('Число строк в файле = ', row_count)
 
-for i in range(3194, row_count + 1):
+for i in range(2, row_count + 1):
     print("Строка", i)
     try:
-        link_xml = ws.cell(i, 45).hyperlink.display  # 46 строка это ссылка на xml
+        link_xml = ws.cell(i, column_link_on_html).hyperlink.display  # 46 строка это ссылка на xml
         print("link_xml ", link_xml)
-        kadastr_num_file = ws.cell(i, 4).value
+        kadastr_num_file = ws.cell(i, column_kadastr).value
         kadastr_num_file = kadastr_num_file.replace(':', "_")
         print('Кадастровый номер ', kadastr_num_file)
         saved_xml_file = str(f'file_html\\{kadastr_num_file}.html')
